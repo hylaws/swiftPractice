e5be0cc4ecc74348a1bb5db51eedd4ce33ea2f1b
healsgoclues <info@healsgoclues.com>
2016-08-21 16:11:46 +0900

    005_UIImageView

--- swiftPractice/ViewController.swift
+++ swiftPractice/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // define myImageView
+    private var myImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //create UIImageView
+        myImageView = UIImageView(frame: CGRectMake(0, 0, self.view.bounds.width, 120))
+        
+        //set image
+        let myImage = UIImage(named: "logo")
+        
+        //add image to myImageView
+        myImageView.image = myImage
+        
+        //set position of myImageView
+        myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y: 200.0)
+        
+        //add UIImageView to view
+        self.view.addSubview(myImageView)
+        
     }
 
     override func didReceiveMemoryWarning() {
