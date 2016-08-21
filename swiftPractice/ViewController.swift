4292fc14b6940a1f724b57f84b87c251ea6b438d
healsgoclues <info@healsgoclues.com>
2016-08-21 17:32:57 +0900

    006_UITableView

--- swiftPractice/ViewController.swift
+++ swiftPractice/ViewController.swift
@@ -8,17 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//inherit UITableViewDelegate, UITableViewDataSource
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // set array for table
+    private let myItems:NSArray = ["hoge","fuga","piyo"]
+    private var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        
+        myTableView.dataSource = self
+        
+        myTableView.delegate = self
+        
+        self.view.addSubview(myTableView)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("Num:\(indexPath.row)")
+        print("value: \(myItems[indexPath.row])")
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItems.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        cell.textLabel!.text = "\(myItems[indexPath.row])"
+        return cell
+        
+    }
+   
 
 
 }
