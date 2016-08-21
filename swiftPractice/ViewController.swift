cab459974d78f7bf21c9d243580b7c1417f1ca37
healsgoclues <info@healsgoclues.com>
2016-08-22 00:53:39 +0900

    009_UIWebView

--- swiftPractice/ViewController.swift
+++ swiftPractice/ViewController.swift
@@ -8,16 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// add UIWebViewDelegate
+class ViewController: UIViewController, UIWebViewDelegate {
 
+    // define myWebView
+    private var myWebView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // get statusBar height
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        // get frame height
+        let displayHeight: CGFloat = self.view.frame.height
+        // get frame width
+        let displayWidth: CGFloat = self.view.frame.width
+        // create myWebView
+        myWebView = UIWebView()
+        
+        // set delegate
+        myWebView.delegate = self
+        
+        // set WebView size and position
+        myWebView.frame = CGRectMake(0, barHeight, displayWidth, displayHeight - barHeight)
+
+
+        
+        // create url
+        let url: NSURL = NSURL(string: "http://www.apple.com")!
+        
+        // make request
+        let request: NSURLRequest = NSURLRequest(URL: url)
+        
+        // set request to myWebView
+        myWebView.loadRequest(request)
+        
+        // add myWebView to view
+        self.view.addSubview(myWebView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+
+    func webViewDidFinishLoad(webView: UIWebView) {
+        print("webViewDidFinishLoad")
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        print("webViewDidStartLoad")
     }
 
 
