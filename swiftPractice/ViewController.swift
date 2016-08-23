a4ef3f4ee40450e4dc6ae07e777cee347fc63671
healsgoclues <info@healsgoclues.com>
2016-08-23 22:14:27 +0900

    016_UIScrollView

--- swiftPractice/ViewController.swift
+++ swiftPractice/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var myScrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //create scroll view
+        myScrollView = UIScrollView()
+        
+        //set size of ScrollView
+        myScrollView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
+        
+        //create image
+        let myImage = UIImage(named: "sample")!
+        
+        //create image view
+        let myImageView = UIImageView()
+        
+        //set image to myImageView
+        myImageView.image = myImage
+        
+        //set myImageView frame
+        myImageView.frame = CGRectMake(0, 0, myImage.size.width, myImage.size.height)
+        
+        //set image view to ScrollView
+        myScrollView.addSubview(myImageView)
+        
+        // set content size to ScrollView
+        myScrollView.contentSize = CGSizeMake(myImageView.frame.size.width, myImageView.frame.size.height)
+        
+        //set ScrollView to view
+        self.view.addSubview(myScrollView)
+        
     }
 
     override func didReceiveMemoryWarning() {
